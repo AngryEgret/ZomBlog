c4db4105f2e5f864112b23e64ac7a6efd6b531e6
Ryan Greget <rxgreget@ussjcpln307668-010054016036.apollogrp.edu>
2012-04-06 12:35:06 -0700

    removing syntax highlighting, adding showdown hilghlight.js

--- lib/post.rb
+++ lib/post.rb
@@ -54,27 +54,7 @@ class Post < Sequel::Model
   ########
 
   def to_html(markdown)
-	  out = []
-	  noncode = []
-	  code_block = nil
-	  markdown.split("\n").each do |line|
-		  if !code_block and line.strip.downcase == '<code>'
-			  out << Maruku.new(noncode.join("\n")).to_html
-			  noncode = []
-			  code_block = []
-		  elsif code_block and line.strip.downcase == '</code>'
-			  convertor = Syntax::Convertors::HTML.for_syntax "ruby"
-			  highlighted = convertor.convert(code_block.join("\n"))
-			  out << "<code>#{highlighted}</code>"
-			  code_block = nil
-		  elsif code_block
-			  code_block << line
-		  else
-			  noncode << line
-		  end
-	  end
-	  out << Maruku.new(noncode.join("\n")).to_html
-	  out.join("\n")
+	  Maruku.new(markdown).to_html_document
   end
 
   def split_content(string)
